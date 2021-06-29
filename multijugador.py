ab6adc569fee8c14df2b5ad17a65a02f9cb0e5c1
abrilkatherine <abrilkatherinec@gmail.com.ar>
2021-06-28 21:57:44 -0300

    "fix bug + mensaje"

--- multijugador.py
+++ multijugador.py
@@ -10,7 +10,7 @@ def solicitar_nombre(nombres_jugadores):
 
     while not nombre_correcto:
         repite_nombre = False
-        nombre_jugador = input("Ingrese su nombre: ")
+        nombre_jugador = input("Ingrese su nombre (Presione enter para salir): ")
 
         for nombre in nombres_jugadores:
             if nombre.lower() == nombre_jugador.lower():
@@ -25,24 +25,26 @@ def solicitar_nombre(nombres_jugadores):
 
 def solicitar_nombres_jugadores():
     """
-    Solicita el ingreso de los nombres de los jugadores. Valida que no sean mas de 5 y que no se repitan.
+    Solicita el ingreso de los nombres de los jugadores. Valida que no sean más de 5 y que no se repitan.
     Devuelve una lista con los nombres.
     """
     nombres_jugadores = []
     numero_jugador = 1
+
     print(f"Jugador {numero_jugador}")
     nombre = solicitar_nombre(nombres_jugadores)
 
     while nombre != "" and numero_jugador <= 5:
         numero_jugador += 1
         nombres_jugadores.append(nombre)
-        print(f"Jugador {numero_jugador}")
-        nombre = solicitar_nombre(nombres_jugadores)
+
+        if numero_jugador <= 5:
+            print(f"Jugador {numero_jugador}")
+            nombre = solicitar_nombre(nombres_jugadores)
 
     return nombres_jugadores
 
-
-#print(solicitar_nombres_jugadores())
+print(solicitar_nombres_jugadores())
 
 
 def asignar_turno_jugadores(nombres_jugadores, ganador):
@@ -69,7 +71,7 @@ def informar_turnos_jugadores(nombres_jugadores):
         #print(f"El jugador {nombres_jugadores[i]} tiene el turno {i + 1}")
         print(f"Turno {i + 1}: {nombres_jugadores[i]}")
 
-informar_turnos_jugadores(["didy", "facu", "ale"])
+#informar_turnos_jugadores(["didy", "facu", "ale"])
 
 def asignar_palabras_jugadores():
     """
