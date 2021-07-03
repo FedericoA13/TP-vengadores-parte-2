b71347483ecae9b12502c9fa7aec1479a78abfee
MoronoMartin <84361036+MoronoMartin@users.noreply.github.com>
2021-07-03 03:31:52 -0300

    etapa-8 recarga csv

--- integracion.py
+++ integracion.py
@@ -1,57 +1,75 @@
-import diccionario
+import palabrasmenos
 import ahorcado
 import constantes as const
+import os
+
+archivo_cuentos = open('Cuentos.txt', 'r')
+archivo_noches = open('Las 1000 Noches y 1 Noche.txt', 'r')
+archivo_arania = open('La araña negra - tomo 1.txt', 'r')
+
+
+def recargar_csv():
+    lista_archivos = [archivo_cuentos, archivo_noches, archivo_arania]
+    dicc = palabrasmenos.creacion_de_dicc(lista_archivos)
+    dicc_ordenado = palabrasmenos.ordenar_dicc(dicc)
+    palabrasmenos.creacion_texto(dicc_ordenado)
+
+    archivo_cuentos.close()
+    archivo_noches.close()
+    archivo_arania.close()
+
+
+def preguntar_recarga_csv():
+    if not os.path.exists("palabras.csv"):
+        recargar_csv()
+        print("El archivo de palabras no existia y lo hemos creado!")
+        jugar_multiples_partidas()
+    else:
+        recargar_palabras = input("Desea recargar el archivo de palabras?: ")
+        if recargar_palabras.lower() == "si": #TODO: AGREGAR VALIDACION SI SE INGRESA ALGO DISTINTO DE SI
+            recargar_csv()
+            print("Hemos creado el archivo de palabras!")
+            jugar_multiples_partidas()
+        else:
+            jugar_multiples_partidas()
 
 
 def seleccion_palabra(desea_letras):
-    """
-    Autor: Federico Aldrighetti.
 
-    Esta función pregunta al usuario si quiere jugar con una cantidad determinada de letras. Si dice que sí,
-    le pide que especifique la cantidad y le da la palabra con esa longitud validada. En caso contrario, le
-    proporciona una palabra con una longitud al azar.
-    """
-
-    dicc = diccionario.devolver_diccionario()
+    archivo_palabras = open('palabras.csv', 'r')
+    lista_palabras = palabrasmenos.generar_palabras_candidatas(archivo_palabras)
 
     if desea_letras.lower() == 'si':
         cant_letras = input('Cuantas letras? ')
-        while not cant_letras.isnumeric() or diccionario.elegir_palabra(dicc, int(cant_letras)) == None:
+        palabra_adivinar = palabrasmenos.elegir_palabra(lista_palabras, int(cant_letras))
+
+        while not cant_letras.isnumeric() or palabra_adivinar == None:
+
             if not cant_letras.isnumeric():
-                cant_letras = input('Ingrese cantidad de letras correcta: ')
-            elif diccionario.elegir_palabra(dicc, int(cant_letras)) == None:
+                cant_letras = input('Ingrese cantidad de letras correcta: ') #TODO: VER ERROR SI PONES STRING, y si sacas el int queda tildado
+            elif palabra_adivinar == None:
                 cant_letras = input(
                     f'No hay palabras con esa longitud. Elige una longitud entre {const.LONGITUD_MINIMA_PALABRA} y {const.LONGITUD_MAXIMA_PALABRA}: ')
 
-        palabra_adivinar = diccionario.elegir_palabra(dicc, int(cant_letras))
+            palabra_adivinar = palabrasmenos.elegir_palabra(lista_palabras, int(cant_letras)) #TODO: VER ERROR SI PONES STRING, y si sacas el int queda tildado
 
     elif desea_letras.lower() == 'no':
-        palabra_adivinar = diccionario.elegir_palabra(dicc)
+        palabra_adivinar = palabrasmenos.elegir_palabra(lista_palabras)
 
     else:
         palabra_adivinar = seleccion_palabra(input(const.INTRODUZCA_COMANDO_DE_NUEVO))
 
     return palabra_adivinar
 
+    archivo_palabras.close()
+
 
 def jugar_una_partida():
-    """
-    Autor: Facundo Sanso.
-
-    La función inicializa la partida
-    """
     palabra_a_adivinar = seleccion_palabra(input(const.DESEA_LETRAS))
     return ahorcado.jugar_ahorcado(palabra_a_adivinar)
 
 
 def jugar_multiples_partidas():
-    """
-    Autor: Facundo Sanso.
-
-    Esta función aparece al finalizar una partida. Le da una opción al usuario de seguir jugando. En caso de
-    decir que no, devuelve el puntaje final y un mensaje de despedida.
-    En caso de que la respuesta sea positiva, el programa inicializará una partida nueva.
-    """
     puntaje = jugar_una_partida()
 
     seguir_jugando = input(f"\n{const.SEGUIR_JUGANDO}")
@@ -64,4 +82,9 @@ def jugar_multiples_partidas():
     print(const.MENSAJE_DESPEDIDA)
 
 
-jugar_multiples_partidas()
+preguntar_recarga_csv()
+
+
+
+
+
